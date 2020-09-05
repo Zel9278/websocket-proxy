40fb0487155df135b9b0c7fc4b7f323eb82f044f
Zel <43949163+Zel9278@users.noreply.github.com>
2020-09-06 08:04:36 +0900

    tw

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var WebSocket = require('ws');
+/*var WebSocket = require('ws');
 var {parse:parseQueryString} = require('query-string');
 var ws = new WebSocket.Server({
 	port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080
@@ -6,10 +6,6 @@ var ws = new WebSocket.Server({
 var conncount = 0;
 ws.on('connection', function (cws, req){
 	var number = conncount++;
-	/*console.log(`New connection #${number} from ${req.connection.remoteAddress} with ${req.url}`);
-	cws.on('close', function(){
-		console.log(`Closing connection from ${req.connection.remoteAddress}`);
-	});*/
 
 	var querystring = req.url.substr(1);
 	if (!querystring) return cws.close();
@@ -54,9 +50,11 @@ ws.on('connection', function (cws, req){
 });
 
 setInterval(() => console.clear(), 500);
+*/
 
-/*process.on('uncaughtException', (err) => {
-	console.log("UnhandledException: " + err);
-});*/
+require("http-proxy").createServer({
+	target: 'ws://www.multiplayerpiano.com:443',
+	ws: true
+}).listen(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
 
 undefined;
